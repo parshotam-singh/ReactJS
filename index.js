25fc3066ebeccc46027201bf38544ab395e60d76
parshotam <parshotam.1100@gmail.com>
2023-02-24 02:24:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HooksP from "./HooksP";
+import './index.css';
+
+//import Time from './Time';
+
+ReactDOM.render(
+  <>
+  <HooksP/>
+  </>,document.getElementById('root')
+);
