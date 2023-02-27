56cdfffb15eb82c52a6abe7e6fe9a27a494ee2f9
parshotam <parshotam.1100@gmail.com>
2023-02-27 23:25:56 +0530

    React_Forms_basics

---
+++ React_Forms/Forms.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const Forms=()=>{
+
+    const [name,setName]=useState();
+    const[fullName,setFullName]=useState();
+
+    const inputEvent=(event)=>{
+         //console.log(event.target.value);
+         setName(event.target.value);
+    }
+    const onSubmit=()=>{
+        setFullName(name);
+    }
+    return(
+        <>
+        <div>
+        <h1>Hello {fullName}</h1>
+        <input type="text" placeholder="Enter your Name" onChange={inputEvent} value={name}></input>
+        <button onClick={onSubmit}>Click Me</button>
+        </div>   
+        </>
+    )
+}
+
+export default Forms;
---
+++ React_Forms/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+
+import Forms from './Forms';
+
+ReactDOM.render(
+  <>
+  <Forms/>
+  </>,document.getElementById('root')
+);
